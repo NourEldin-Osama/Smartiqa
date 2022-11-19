e60b86b21e1dab7e16c2ec597ac8e4207a7f4548
Yousr <yousr.ahmed.muhammed@gmail.com>
2022-11-19 17:23:47 +0200

    Fix Sort for __str__ in table

--- Smart/tables.py
+++ Smart/tables.py
@@ -1,12 +1,13 @@
 import django_tables2 as tables
+from django.db.models import Value
+from django.db.models.functions import Concat, Lower, Trim, Coalesce, NullIf
 from django_tables2 import A
 
 from .models import *
 
 ATTRIBUTES = {"class": "table table-striped dt-responsive table-hover table-bordered",
               "thead": {"class": "text-capitalize custom-table-head"},
-              "tbody": {"class": "table-group-divider custom-table-body"},
-              "th": {"class": ""}}
+              "tbody": {"class": "table-group-divider custom-table-body"}, "th": {"class": ""}}
 
 
 class UserTable(tables.Table):
@@ -18,14 +19,21 @@ class UserTable(tables.Table):
 
 class InstructorTable(tables.Table):
     user = tables.Column(accessor='user', verbose_name="full name")
-    username = tables.Column(accessor='user__username', verbose_name="username",
-                             linkify=("instructor", [A("user__username")]))  # (viewname, args)
+    username = tables.Column(accessor='user.username', verbose_name="username",
+                             linkify=("instructor", [A("user.username")]))  # (viewname, args)
     major = tables.Column(accessor='user.major', verbose_name="major")
+
+    def order_user(self, queryset, is_descending):
+        string_full_name = Trim(Lower(Concat(A("user__first_name"), Value(' '), A("user__last_name"))))
+        string_username = Trim(Lower(A("user__username")))
+        queryset = queryset.annotate(name=Coalesce(NullIf(string_full_name, Value("")), string_username)).order_by(
+            ("-" if is_descending else "") + "name")
+        return queryset, True
 
     class Meta:
         model = Instructor
         attrs = ATTRIBUTES
-        orderable = False
+        orderable = True
         fields = ('user', 'job_title', 'username', 'major')
         sequence = ('user', 'job_title', 'username', 'major')
 
